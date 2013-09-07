4634835f2690da4989d825494a9417fd382e6e6a
ancrod <michael.abbey@gmail.com>
2013-09-07 10:33:55 +1000

    application inf application js

---
+++ application.js
@@ -0,0 +1,10 @@
+$(document).ready(function)() {
+  $(".boxes").on("mouseenter", function() {
+    
+    $(this).closest(".txtcenter").find("h2").slideDown;
+    $(".boxes").remove();
+    
+  
+  });
+
+ });   
