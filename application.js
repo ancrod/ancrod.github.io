a45e1a568aa5cb856003bf3d6c11387f049ac96a
ancrod <michael.abbey@gmail.com>
2013-09-18 21:38:32 +1000

    Correction of jquery

--- application.js
+++ application.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  $("box").on("mouseenter", function() {
-   $(this).addClass("txtcenter");
-     console.log("hokey", this);
-  });   
-});       
-       $(".box").on("mouseleave", function() {
-         $(".main").append("mainp");
-           console.log("pokey", this);
-    
+  
+  $(".box").on("mouseenter", function() {
+    // $(this).addClass("txtcenter");
+    console.log("hokey", this);
+  });
+  
+  $(".box").on("mouseleave", function() {
+    // $(".main").append("mainp");
+    console.log("pokey", this);
   });
  
+});       
