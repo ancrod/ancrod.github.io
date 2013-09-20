bb92d4078071954a136d3cbae156530a4ac63b10
ancrod <michael.abbey@gmail.com>
2013-09-20 13:41:50 +1000

    Jquery play

--- application.js
+++ application.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   
   $(".box").on("mouseenter", function() {
-    // $(this).addClass("txtcenter");
+   $(".main h1").text("put the text here");
     
     console.log("hokey", this);
   });
   
   $(".box").on("mouseleave", function() {
-    // $(".main").append("mainp");
+    $(".main h1")
     console.log("pokey", this);
   });
  
