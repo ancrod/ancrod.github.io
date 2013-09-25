d4ef966a8550edaaa2150df038fbbe774a5b2284
ancrod <michael.abbey@gmail.com>
2013-09-26 04:52:29 +1000

    ongoing play

--- application.js
+++ application.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
-  $(".box").hover(function() {
-    $(".
-  
-   
+    $(".box").on("mouseenter", function() {
+      $(".main h1").addClass("h2 p");
+      $(".main").removeClass("h1 p");
     
-    console.log("hokey", this);
+      console.log("hokey", this);
+  });
+    
+    // $(".box").on("mouseenter", function() {
+      // $(".txtcenter").find("h2").hover();
+      // $(".txt").find("p").hover();
+      // console.log("mokey", this);
+    // });
+    
+    $(".box").on("mouseleave", function() { 
+      $(".main .mainp").addClass();
+      $(".txtcenter").removeClass();
+      console.log("pokey", this);
+    });
   });
   
-  $(".box").on("mouseleave", function() {
-    $(".main h1")
-    console.log("pokey", this);
-  });
+
+ // $(".main h1").text("put the text here"); 
+  
+    // $(".main h1")
+   //$(".mainp").removeClass();  
+  // });
+ //$(".boxes .bc").hide();
+// });       
+  // $(".box").on("mouseenter", function() {
  
-});       
-// $(".box").on("mouseenter", function() {
-// $(".main h1").text("put the text here");
+
+// $(".txtcenter .bc").show();
+// $(".main").hide();
